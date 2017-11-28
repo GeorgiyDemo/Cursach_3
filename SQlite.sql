7a36a414dda599ddccdeee695b3edb3ce547f008
Georgiy Demenchuk <demenchuk.george@mail.ru>
2017-11-28 20:49:23 +0300

    Пример SQL для интеграции в прогу

---
+++ SQlite.sql
@@ -0,0 +1,28 @@
+PRAGMA foreign_keys=on;
+CREATE TABLE IF NOT EXISTS students(
+  ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  name TEXT, score REAL, priority TEXT, form_sudy TEXT, major TEXT, original TEXT, form_pay TEXT, date TEXT);
+
+CREATE TABLE data(
+
+	ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	student_id INTEGER UNIQUE NOT NULL,
+	adress TEXT,
+	e_mail TEXT,
+	telephone TEXT,
+	date_birth TEXT,
+FOREIGN KEY (student_id) REFERENCES students(ID));
+
+CREATE TABLE parents(
+
+	ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	student_id INTEGER NOT NULL,
+	name TEXT,
+	adress TEXT,
+	e_mail TEXT,
+	telephone TEXT,
+FOREIGN KEY (student_id) REFERENCES students(ID));
+
+INSERT INTO students VALUES(NULL,"Петров Александр",5.0,"да","очная","ПКС","оригинал","бюджет","20.10.2016");
+INSERT INTO data VALUES(NULL,1,"ул Ленина","student@e-mail.com","+79945969459","19.06.1999");
+INSERT INTO parents VALUES(NULL,1,"ул Ленина","Петров Иван","parent@e-mail.com","+74966349493");
